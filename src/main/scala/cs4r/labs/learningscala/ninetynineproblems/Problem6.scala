82ed15d6c947d4a1c948cccc9f179cd75311f999
César Aguilera <cesar.aguilera.p@gmail.com>
2016-05-16 21:33:51 +0200

    added 1 more problem with its test

---
+++ src/main/scala/cs4r/labs/learningscala/ninetynineproblems/Problem6.scala
@@ -0,0 +1,8 @@
+package cs4r.labs.learningscala.ninetynineproblems
+
+/**
+  * Created by cs4r on 16/05/16.
+  */
+object Problem6 {
+
+}
---
+++ src/test/scala/cs4r/labs/learningscala/ninetynineproblems/Problem6Test.scala
@@ -0,0 +1,10 @@
+package cs4r.labs.learningscala.ninetynineproblems
+
+import org.scalatest.FlatSpec
+
+/**
+  * Created by cs4r on 16/05/16.
+  */
+class Problem6Test extends FlatSpec {
+
+}
