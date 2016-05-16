dd868a594b941060ee6c721d5f9e3b3c2365f753
César Aguilera <cesar.aguilera.p@gmail.com>
2016-05-16 21:34:15 +0200

    added 1 more problem with its test

--- src/main/scala/cs4r/labs/learningscala/ninetynineproblems/Problem6.scala
+++ src/main/scala/cs4r/labs/learningscala/ninetynineproblems/Problem6.scala
@@ -1,8 +1,20 @@
 package cs4r.labs.learningscala.ninetynineproblems
 
+import scala.annotation.tailrec
+
 /**
-  * Created by cs4r on 16/05/16.
+  * Find out whether a list is a palindrome.
   */
 object Problem6 {
+  def isPalindrome[T](list: List[T]): Boolean = {
+    @tailrec
+    def isPalindromeTailRecursive(b: Boolean, list: List[T]): Boolean = (b, list) match {
+      case (false, list) => false
+      case (true, Nil) => true
+      case (true, x :: Nil) => true
+      case (true, x :: tail) => isPalindromeTailRecursive(x == tail.last, tail.init)
+    }
 
+    isPalindromeTailRecursive(true, list)
+  }
 }
--- src/test/scala/cs4r/labs/learningscala/ninetynineproblems/Problem6Test.scala
+++ src/test/scala/cs4r/labs/learningscala/ninetynineproblems/Problem6Test.scala
@@ -1,10 +1,66 @@
 package cs4r.labs.learningscala.ninetynineproblems
 
-import org.scalatest.FlatSpec
+import org.scalatest.{FlatSpec, GivenWhenThen}
 
-/**
-  * Created by cs4r on 16/05/16.
-  */
-class Problem6Test extends FlatSpec {
+
+class Problem6Test extends FlatSpec with GivenWhenThen {
+
+  behavior of "isPalindrome"
+
+  it must "return true when Nil is provided" in {
+    Given("Nil")
+    When("isPalindrome is called")
+    Then("the result must be true")
+    assert(Problem6.isPalindrome(Nil))
+
+  }
+
+
+  it must "return true when an empty is provided" in {
+    Given("an empty list")
+    When("isPalindrome is called")
+    Then("the result must be true")
+    assert(Problem6.isPalindrome(List()))
+  }
+
+  it must "return true when a one-element list is provided" in {
+    Given("a list of one element")
+    When("isPalindrome is called")
+    Then("the result must be true")
+    assert(Problem6.isPalindrome(List(1)))
+  }
+
+  it must "return false when List(1,2) is provided" in {
+    Given("the List(1,2)")
+    val list = List(1, 2)
+    When("isPalindrome is called")
+    Then("the result must be false")
+    assert(Problem6.isPalindrome(list) == false)
+  }
+
+  it must "return true when List(1,2,1) is provided" in {
+    Given("the List(1,2,1)")
+    val list = List(1, 2, 1)
+    When("isPalindrome is called")
+    Then("the result must be true")
+    assert(Problem6.isPalindrome(list))
+  }
+
+  it must "return true when List(1,2,2,1) is provided" in {
+    Given("the List(1,2,2,1)")
+    val list = List(1, 2, 2, 1)
+    When("isPalindrome is called")
+    Then("the result must be true")
+    assert(Problem6.isPalindrome(list))
+  }
+
+  it must "return false when List(1,2,3,1) is provided" in {
+    Given("the List(1,2,3,1)")
+    val list = List(1, 2, 3, 1)
+    When("isPalindrome is called")
+    Then("the result must be false")
+    assert(Problem6.isPalindrome(list) == false)
+  }
+
 
 }
