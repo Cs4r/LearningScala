596493084e14ac9e5e336f1201863577fa32616c
César Aguilera <cesar.aguilera.p@gmail.com>
2016-06-04 21:09:59 +0200

    resolved exercise 2.2 of Functional Programming in Scala

---
+++ src/main/scala/cs4r/labs/learningscala/fpinscala/chapter2/Exercises.scala
@@ -0,0 +1,45 @@
+package cs4r.labs.learningscala.fpinscala.chapter2
+
+/**
+  * Created by cs4r on 4/06/16.
+  */
+object Exercises extends App {
+
+
+  println(fib(23))
+
+  private val ints: Array[Int] = Array(1, 2, 3, 4, 4)
+
+  println(isSorted(ints, (a: Int, b: Int) => a < b))
+  println(isSorted(ints, (a: Int, b: Int) => a <= b))
+  println(isSorted(ints.reverse, (a: Int, b: Int) => a > b))
+  println(isSorted(ints.reverse, (a: Int, b: Int) => a >= b))
+
+
+  def fib(n: Int): Int = {
+
+    def fibTailRecursive(n: Int, prev: Int, next: Int): Int = n match {
+      case 0 => prev
+      case _ => fibTailRecursive(n - 1, next, prev + next)
+    }
+
+    return fibTailRecursive(n, 0, 1)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+
+
+    def loop(n: Int): Boolean = {
+      if (n == as.length)
+        true
+      else if (ordered(as(n-1), as(n)))
+        loop(n + 1)
+      else false
+    }
+
+    if (as.length < 2)
+      true
+    else
+      loop(1)
+  }
+}
