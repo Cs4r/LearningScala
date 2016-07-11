c776c7813324050b7369dd7c9840ef4ed7d26065
César Aguilera <cesar.aguilera.p@gmail.com>
2016-07-11 22:37:13 +0200

    read chapter 4...

--- src/main/scala/cs4r/labs/learningscala/fpinscala/chapter4/Option.scala
+++ src/main/scala/cs4r/labs/learningscala/fpinscala/chapter4/Option.scala
@@ -56,4 +56,10 @@ object Option {
     case head :: tail => head flatMap (hh => sequence(tail) map (tt => (hh :: tt)))
   }
 
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =  ???
+
+  sealed trait Either[+E, -A]
+  case class Left[+E](value: E) extends Either[E, Nothing]
+  case class Right[+A](value: A) extends Either[Nothing, A]
+
 }
